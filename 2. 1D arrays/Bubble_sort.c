168c90800e780a4c4baed2fdfabd48d9965db1e9
Leon Vincelj <leonvinc7@gmail.com>
2025-02-17 22:20:40 +0100

    C

---
+++ 2. 1D arrays/Bubble_sort.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+/*
+Program koji radi bubble sort brojeva
+*/
+int main()
+{
+    int niz[100];
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &niz[i]);
+    }
+    int bilaZamjena;
+    do {
+        bilaZamjena = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (niz[i] > niz[i + 1]) {
+                int temp = niz[i];
+                niz[i] = niz[i + 1];
+                niz[i + 1] = temp;
+                bilaZamjena = 1;
+            }
+        }
+    } while (bilaZamjena);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", niz[i]);
+    }
+    return 0;
+}
+
